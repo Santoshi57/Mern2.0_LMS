1df2a756df559a4a7964dadf8f169833b37ed182
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2024-03-03 20:57:40 +0545

    changes

---
+++ src/pages/editBook/EditBook.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const EditBook = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditBook
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+
+const Home = () => {
+  return (
+  <>
+  <Navbar/>
+  </>
+  )
+}
+
+export default Home
